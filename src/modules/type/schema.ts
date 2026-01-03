307ef808208c558217429029be786335e4cb8620
M Haidar Hanif <git@mhaidarhanif.com>
2026-01-03 13:59:36 +0700

    day 28

--- src/modules/type/schema.ts
+++ src/modules/type/schema.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 
-export const phoneCreateSchema = z.object({
+export const PhoneSchema = z.object({
+  id: z.number().positive(),
   brand: z.string().min(1),
   model: z.string().min(1),
   slug: z.string().min(1),
@@ -8,9 +9,18 @@ export const phoneCreateSchema = z.object({
   os: z.string().min(1),
   chipset: z.string().min(1),
   releaseYear: z.number().int().min(2000),
+  createdAt: z.date(),
+  updatedAt: z.date(),
 });
 
-export const phoneUpdateSchema = phoneCreateSchema.partial();
+export const PhoneCreateSchema = PhoneSchema.omit({
+  id: true,
+  createdAt: true,
+  updatedAt: true,
+});
 
-export type PhoneCreateInput = z.infer<typeof phoneCreateSchema>;
-export type PhoneUpdateInput = z.infer<typeof phoneUpdateSchema>;
+export const PhoneUpdateSchema = PhoneCreateSchema.partial();
+
+export type Phone = z.infer<typeof PhoneSchema>;
+export type PhoneCreateInput = z.infer<typeof PhoneCreateSchema>;
+export type PhoneUpdateInput = z.infer<typeof PhoneUpdateSchema>;
